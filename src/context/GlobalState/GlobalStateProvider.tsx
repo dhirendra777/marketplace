b8852ba00d37f95ff6e833caec1b1ee167747f56
Dhirendra Naik <dhirendranaik2@gmail.com>
2025-02-19 14:40:42 +0530

    fix(): fix isMobile check to work without refresh

--- src/context/GlobalState/GlobalStateProvider.tsx
+++ src/context/GlobalState/GlobalStateProvider.tsx
@@ -1,10 +1,21 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { GlobalStateContext } from './context';
 import { GlobalStateProviderProps } from './types';
 
 export const GlobalStateProvider: React.FC<GlobalStateProviderProps> = ({ children }) => {
   const [isOpen, setIsOpen] = useState<boolean>(true);
-  const isMobile = window.innerWidth < 768;
+  const [isMobile, setIsMobile] = useState(window.innerWidth < 768);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsMobile(window.innerWidth < 768);
+    };
+    window.addEventListener('resize', handleResize);
+
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
 
   const toggleSidenav = () => {
     setIsOpen((prevState) => !prevState);
